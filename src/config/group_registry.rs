422019d3b9f0669d2e1507d8165f1ab7d2e8bb64
Gabriel Marques <gabrielmrqs.contato@gmail.com>
2022-12-16 10:25:07 -0300

    tomara que nao se percam...

--- src/config/group_registry.rs
+++ src/config/group_registry.rs
@@ -3,12 +3,22 @@ use serenity::framework::StandardFramework;
 #[path = "../commands/misc/misc.rs"]
 mod misc;
 
+#[path = "../data/utils.rs"]
+mod utils;
+
 pub trait FrameworkExtensions {
     fn register_groups(self) -> StandardFramework;
+    fn register_buckets(self) -> StandardFramework;
 }
 
 impl FrameworkExtensions for StandardFramework {
+    fn register_buckets(self) -> StandardFramework {
+        self.bucket("pirocudo",|b| b.check()))
+    }
+
     fn register_groups(self) -> StandardFramework {
         self.group(&misc::MISC_GROUP)
     }
 }
+
+
