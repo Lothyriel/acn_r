82916546a06a83405bfa417756f007b29ad46735
João Xavier <fastjonh@gmail.com>
2022-12-16 20:40:25 -0300

    removendo handlers de teste

--- src/config/event_handler.rs
+++ src/config/event_handler.rs
@@ -2,8 +2,7 @@ use serenity::{
     async_trait,
     model::{
         gateway::Ready,
-        prelude::{InviteCreateEvent, Member, Presence, TypingStartEvent},
-        voice::VoiceState,
+        prelude::{InviteCreateEvent, Member},
     },
     prelude::{Context, EventHandler, Mentionable},
     Error,
@@ -25,43 +24,6 @@ impl EventHandler for AcnHandler {
     async fn ready(&self, _: Context, ready: Ready) {
         println!("Estamos totalmente dentro! {}", ready.user.name);
     }
-
-    async fn typing_start(&self, ctx: Context, event: TypingStartEvent) {
-        handle_typing_start(ctx, event).await.log();
-    }
-
-    async fn voice_state_update(&self, ctx: Context, state: VoiceState) {
-        handle_voice_state_update(ctx, state).await.log();
-    }
-
-    async fn presence_update(&self, ctx: Context, new_data: Presence) {
-        handle_presence_update(ctx, new_data).await.log();
-    }
-}
-
-async fn handle_presence_update(ctx: Context, new_data: Presence) -> Result<(), Error> {
-    let user = new_data
-        .user
-        .avatar
-        .ok_or_else(|| Error::Other("Não encontrei o usuário"))?;
-
-    println!("{} {:?}", user, new_data.activities);
-
-    let dm = new_data.user.id.create_dm_channel(&ctx.http).await.unwrap();
-
-    dm.say(&ctx.http, "Vai tomar no cu").await.unwrap();
-
-    Ok(())
-}
-
-async fn handle_typing_start(ctx: Context, event: TypingStartEvent) -> Result<(), Error> {
-    let channel = event.channel_id;
-
-    channel
-        .say(&ctx.http, "Estou pressentindo merda...")
-        .await?;
-
-    Ok(())
 }
 
 async fn handle_invite_create(ctx: Context, event: InviteCreateEvent) -> Result<(), Error> {
@@ -72,35 +34,6 @@ async fn handle_invite_create(ctx: Context, event: InviteCreateEvent) -> Result<
 
     let response = format!("{} Está chamando randoms....", inviter.mention());
     channel.say(&ctx.http, response).await?;
-
-    Ok(())
-}
-
-async fn handle_voice_state_update(ctx: Context, state: VoiceState) -> Result<(), Error> {
-    let member = state
-        .member
-        .ok_or_else(|| Error::Other("Não encontrei o membro"))?;
-
-    let action = if state.self_mute {
-        "mutado"
-    } else {
-        "desmutado"
-    };
-
-    let guild_id = state
-        .guild_id
-        .ok_or_else(|| Error::Other("Não encontrei a guilda"))?;
-
-    let channels = guild_id.channels(&ctx.http).await?;
-
-    let text_channel = channels
-        .values()
-        .filter(|c| c.is_text_based())
-        .min_by(|a, b| a.position.cmp(&b.position))
-        .ok_or_else(|| Error::Other("Não achei um canal"))?;
-
-    let response = format!("{} está {}", member.mention(), action);
-    text_channel.say(&ctx.http, response).await?;
 
     Ok(())
 }
