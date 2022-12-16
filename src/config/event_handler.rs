65bf7b31f8ed34897a8dfbdc82edcae9c5243d2b
João Xavier <fastjonh@gmail.com>
2022-12-16 13:11:41 -0300

    implementados handlers

--- src/config/event_handler.rs
+++ src/config/event_handler.rs
@@ -2,32 +2,115 @@ use serenity::{
     async_trait,
     model::{
         gateway::Ready,
-        prelude::{InviteCreateEvent, TypingStartEvent},
+        prelude::{InviteCreateEvent, Member, Presence, TypingStartEvent},
         voice::VoiceState,
     },
-    prelude::{Context, EventHandler},
+    prelude::{Context, EventHandler, Mentionable},
+    Error,
 };
 pub struct AcnHandler;
 
 #[async_trait]
 impl EventHandler for AcnHandler {
+    async fn guild_member_addition(&self, ctx: Context, new_member: Member) {
+        handle_guild_member_addition(ctx, new_member).await.log();
+    }
+
+    async fn invite_create(&self, ctx: Context, event: InviteCreateEvent) {
+        handle_invite_create(ctx, event).await.log();
+    }
+
     async fn ready(&self, _: Context, ready: Ready) {
         println!("Estamos totalmente dentro! {}", ready.user.name);
     }
 
-    async fn invite_create(&self, _ctx: Context, event: InviteCreateEvent) {
-        println!("Estão chamando randoms.... no {}", event.channel_id)
+    async fn typing_start(&self, ctx: Context, event: TypingStartEvent) {
+        handle_typing_start(ctx, event).await.log();
     }
 
-    async fn typing_start(&self, _ctx: Context, _: TypingStartEvent) {        
-        println!("Estou pressentindo merda...")
+    async fn voice_state_update(&self, ctx: Context, state: VoiceState) {
+        handle_voice_state_update(ctx, state).await.log();
     }
 
-    async fn voice_state_update(&self, _ctx: Context, state: VoiceState) {
-        let member = state.member.expect("Usuário Válido");
+    async fn presence_update(&self, ctx: Context, new_data: Presence) {
+        println!("{} {:?}", new_data.user.avatar.unwrap(), new_data.activities);
 
-        let action = if state.self_mute { "Mutou" } else { "Desmutou" };
+        let dm = new_data.user.id.create_dm_channel(&ctx.http).await.unwrap();
 
-        println!("{} {}", member.user.name, action)
+        dm.say(&ctx.http, "Vai tomar no cu").await.unwrap();
+    }
+}
+
+async fn handle_typing_start(ctx: Context, event: TypingStartEvent) -> Result<(), Error> {
+    let channel = event.channel_id;
+
+    channel
+        .say(&ctx.http, "Estou pressentindo merda...")
+        .await?;
+
+    Ok(())
+}
+
+async fn handle_invite_create(ctx: Context, event: InviteCreateEvent) -> Result<(), Error> {
+    let channel = event.channel_id;
+    let inviter = event
+        .inviter
+        .ok_or_else(|| Error::Other("Não achei quem criou o convite"))?;
+
+    let response = format!("{} Está chamando randoms....", inviter.mention());
+    channel.say(&ctx.http, response).await?;
+
+    Ok(())
+}
+
+async fn handle_voice_state_update(ctx: Context, state: VoiceState) -> Result<(), Error> {
+    let member = state
+        .member
+        .ok_or_else(|| Error::Other("Não encontrei o membro"))?;
+
+    let action = if state.self_mute { "Mutou" } else { "Desmutou" };
+
+    let guild_id = state
+        .guild_id
+        .ok_or_else(|| Error::Other("Não encontrei a guilda"))?;
+
+    let channels = guild_id.channels(&ctx.http).await?;
+
+    let text_channel = channels
+        .values()
+        .filter(|c| c.is_text_based())
+        .min_by(|a, b| a.position.cmp(&b.position))
+        .ok_or_else(|| Error::Other("Não achei um canal"))?;
+
+    let response = format!("{} {}", member.mention(), action);
+    text_channel.say(&ctx.http, response).await?;
+
+    Ok(())
+}
+
+async fn handle_guild_member_addition(ctx: Context, new_member: Member) -> Result<(), Error> {
+    let channels = new_member.guild_id.channels(&ctx.http).await?;
+
+    let text_channel = channels
+        .values()
+        .min_by(|a, b| a.position.cmp(&b.position))
+        .ok_or_else(|| Error::Other("Não achei um canal"))?;
+
+    let response = format!("Novo random detectado: {}", new_member.mention());
+    text_channel.say(&ctx.http, response).await?;
+
+    Ok(())
+}
+
+trait LogErrors {
+    fn log(self);
+}
+
+impl LogErrors for Result<(), Error> {
+    fn log(self) {
+        match self {
+            Ok(_) => return,
+            Err(error) => println!("{}", error),
+        }
     }
 }
