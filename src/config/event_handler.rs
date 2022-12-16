ed00bd54855502a27591dea565a5d2dd4af74ce4
João Xavier <fastjonh@gmail.com>
2022-12-16 09:08:27 -0300

    testando alguns handlers

--- src/config/event_handler.rs
+++ src/config/event_handler.rs
@@ -1,6 +1,10 @@
 use serenity::{
     async_trait,
-    model::gateway::Ready,
+    model::{
+        gateway::Ready,
+        prelude::{InviteCreateEvent, TypingStartEvent},
+        voice::VoiceState,
+    },
     prelude::{Context, EventHandler},
 };
 pub struct AcnHandler;
@@ -9,5 +13,21 @@ pub struct AcnHandler;
 impl EventHandler for AcnHandler {
     async fn ready(&self, _: Context, ready: Ready) {
         println!("Estamos totalmente dentro! {}", ready.user.name);
+    }
+
+    async fn invite_create(&self, _ctx: Context, event: InviteCreateEvent) {
+        println!("Estão chamando randoms.... no {}", event.channel_id)
+    }
+
+    async fn typing_start(&self, _ctx: Context, _: TypingStartEvent) {        
+        println!("Estou pressentindo merda...")
+    }
+
+    async fn voice_state_update(&self, _ctx: Context, state: VoiceState) {
+        let member = state.member.expect("Usuário Válido");
+
+        let action = if state.self_mute { "Mutou" } else { "Desmutou" };
+
+        println!("{} {}", member.user.name, action)
     }
 }
