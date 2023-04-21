d1707e535812303f7b5a22e06e5a84d50872fd28
João Xavier <fastjonh@gmail.com>
2023-04-21 12:00:49 -0300

    inserting mongoClient into serenity data

--- src/application/services/dependency_configuration.rs
+++ src/application/services/dependency_configuration.rs
@@ -1,10 +1,17 @@
+use mongodb::Client;
 use serenity::prelude::TypeMap;
 use tokio::sync::RwLockWriteGuard;
 
 use crate::application::{
+    infra::mongo_client::MongoClient,
     models::{allowed_ids::AllowedIds, appsettings::AppSettings},
 };
 
-pub fn register_dependencies(mut data: RwLockWriteGuard<TypeMap>, settings: AppSettings) {
+pub fn register_dependencies(
+    mut data: RwLockWriteGuard<TypeMap>,
+    settings: AppSettings,
+    mongo_client: Client,
+) {
     data.insert::<AllowedIds>(settings.allowed_ids);
+    data.insert::<MongoClient>(mongo_client);
 }
