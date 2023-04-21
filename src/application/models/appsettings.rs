eebe8694ac8275d22c38f456192ceb43eeb9e7e9
João Xavier <fastjonh@gmail.com>
2023-04-21 11:35:44 -0300

    adicionando configuracoes na appsettings

--- src/application/models/appsettings.rs
+++ src/application/models/appsettings.rs
@@ -3,4 +3,6 @@ use serde::{Deserialize, Serialize};
 #[derive(Deserialize, Serialize, Debug)]
 pub struct AppSettings {
     pub allowed_ids: Vec<u64>,
+    pub mongo_user: String,
+    pub mongo_cluster_url: String
 }
