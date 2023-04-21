ac39f1b48931dcf395a42c4bdcb4f7d463a5bbee
João Xavier <fastjonh@gmail.com>
2023-04-21 12:00:21 -0300

    creating typeMap to use Client on serenity data

--- src/application/infra/mongo_client.rs
+++ src/application/infra/mongo_client.rs
@@ -1,8 +1,9 @@
 use anyhow::{anyhow, Error};
 use mongodb::{options::ClientOptions, Client};
+use serenity::prelude::TypeMapKey;
 use crate::{application::models::appsettings::AppSettings, get_token_bot};
 
-pub async fn create_mongo_client(appsettings: AppSettings) -> Result<Client, Error> {
+pub async fn create_mongo_client(appsettings: &AppSettings) -> Result<Client, Error> {
     let password = get_token_bot()?;
     let connection_string =
         "mongodb+srv://{USER}:{PASSWORD}@{CLUSTER_URL}/?retryWrites=true&w=majority"
@@ -12,4 +13,10 @@ pub async fn create_mongo_client(appsettings: AppSettings) -> Result<Client, Err
 
     let options = ClientOptions::parse(connection_string).await?;
     Client::with_options(options).map_err(|e| anyhow!(e))
+}
+
+pub struct MongoClient;
+
+impl TypeMapKey for MongoClient {
+    type Value = Client;
 }
