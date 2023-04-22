fec46214fca0e28dcca31d7c840b646b31811c11
João Xavier <fastjonh@gmail.com>
2023-04-22 05:55:38 -0300

    renaming

--- src/application/services/appsettings.rs
+++ src/application/services/appsettings.rs
@@ -6,7 +6,7 @@ use crate::application::models::appsettings::AppSettings;
 
 pub const APPSETTINGS_PATH: &str = "./appsettings.json";
 
-pub fn load_appsettings() -> Result<AppSettings, Error> {
+pub fn load() -> Result<AppSettings, Error> {
     let settings_path = fs::read_to_string(APPSETTINGS_PATH)?;
     serde_json::from_str::<AppSettings>(&settings_path).map_err(|e| anyhow!(e))
 }
