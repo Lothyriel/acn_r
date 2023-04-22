c265078d4d42d4967fe70d885a4262f3c891f0d5
João Xavier <fastjonh@gmail.com>
2023-04-22 11:21:26 -0300

    added user entity

---
+++ src/application/models/entities/user.rs
@@ -0,0 +1,26 @@
+use std::fmt::{self, Debug, Display};
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct User {
+    pub id: u64,
+}
+
+#[derive(Debug, Serialize, Deserialize, Copy, Clone)]
+pub enum Activity {
+    Connected,
+    Disconnected,
+    Muted,
+    Unmuted,
+    OpenedStream,
+    ClosedStream,
+    OpenedCamera,
+    ClosedCamera,
+}
+
+impl Display for Activity {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        Debug::fmt(self, f)
+    }
+}
