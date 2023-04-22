7c694bb1a039e48b5265e502763ea709e27dcac7
João Xavier <fastjonh@gmail.com>
2023-04-22 13:03:38 -0300

    renamed and added CommandError entity

--- src/application/models/entities/command.rs
+++ src/application/models/entities/command.rs
@@ -9,3 +9,13 @@ pub struct CommandUse {
     pub name: String,
     pub args: String
 }
+
+#[derive(Serialize, Deserialize)]
+pub struct CommandError {
+    pub guild_id: Option<u64>,
+    pub user_id: u64,
+    pub date: DateTime<Utc>,
+    pub name: String,
+    pub args: String,
+    pub error: String
+}
