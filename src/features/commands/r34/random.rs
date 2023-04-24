42e90815919fe6b2843ce2cc7cc7c7cf589c7290
João Xavier <fastjonh@gmail.com>
2023-04-24 11:59:18 -0300

    saving unfinished work

--- src/features/commands/r34/random.rs
+++ src/features/commands/r34/random.rs
@@ -4,9 +4,15 @@ use serenity::{
     prelude::Context,
 };
 
+use crate::extensions::dependency_ext::Dependencies;
+
 #[command]
-async fn random(ctx: &Context, _msg: &Message, args: Args) -> CommandResult {
-    let message = args.rest();
+async fn random(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+    let now = chrono::Utc::now();
+
+    //let r34_services = ctx.get_dependency::<>().await?;
+
+    let _message = args.rest();
 
     Ok(())
 }
