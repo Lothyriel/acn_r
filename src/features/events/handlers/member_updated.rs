c1f9d2c43ccb7d6842d22ad4e152d9a0cc98b365
João Xavier <fastjonh@gmail.com>
2023-04-24 11:59:09 -0300

    suiting module changes

--- src/features/events/handlers/member_updated.rs
+++ src/features/events/handlers/member_updated.rs
@@ -2,7 +2,9 @@ use anyhow::Error;
 use serenity::{model::prelude::Member, prelude::Context};
 
 use crate::{
-    application::{services::mongo::user_services::UserServices, models::dto::user_services::UpdateNickDto},
+    application::{
+        models::dto::user_services::UpdateNickDto, services::user_services::UserServices,
+    },
     extensions::dependency_ext::Dependencies,
 };
 
@@ -10,13 +12,13 @@ pub async fn handler(ctx: Context, new: Member) -> Result<(), Error> {
     let now = chrono::Utc::now();
     let user_services = ctx.get_dependency::<UserServices>().await?;
 
-    let dto = UpdateNickDto{
+    let dto = UpdateNickDto {
         user_id: new.user.id.0,
         guild_id: Some(new.guild_id.0),
         new_nickname: new.display_name().to_string(),
-        date: now
+        date: now,
     };
-    
+
     user_services.update_nickname(dto).await?;
 
     Ok(())
