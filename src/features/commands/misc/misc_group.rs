08068ae6b64fb2b54f0e54169f8e9a80a023638d
João Xavier <fastjonh@gmail.com>
2023-05-02 21:58:32 -0300

    added command to group

--- src/features/commands/misc/misc_group.rs
+++ src/features/commands/misc/misc_group.rs
@@ -1,8 +1,8 @@
 use serenity::framework::standard::macros::group;
-use crate::features::commands::misc::{att::ATT_COMMAND, stats::STATS_COMMAND};
+use crate::features::commands::misc::{att::ATT_COMMAND, stats::STATS_COMMAND, debug::DEBUG_COMMAND};
 
 #[group]
-#[commands(att, stats)]
+#[commands(att, stats, debug)]
 #[summary = "Miscellaneous"]
 #[description = "Simplesmente comandos miscellaneous..."]
 pub struct Misc;
