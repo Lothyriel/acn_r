5dd05831e6d69a41107a94242661d26c2ca98419
João Xavier <fastjonh@gmail.com>
2023-05-06 17:53:57 -0300

    -adding initial data info to message -receiving target in command -passing target up to service

--- src/features/commands/misc/stats.rs
+++ src/features/commands/misc/stats.rs
@@ -8,7 +8,7 @@ use serenity::{
 };
 
 use crate::{
-    application::{ services::stats_services::StatsServices},
+    application::services::stats_services::StatsServices,
     extensions::{dependency_ext::Dependencies, log_ext::LogErrorsExt},
 };
 
@@ -16,27 +16,38 @@ use crate::{
 #[owners_only]
 #[only_in(guilds)]
 #[description("Mostra os stats dos membros deste server")]
-async fn stats(ctx: &Context, msg: &Message, _args: Args) -> CommandResult {
+async fn stats(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    let target = args.single::<u64>().ok();
+
     let service = ctx.get_dependency::<StatsServices>().await?;
 
     let guild_id = msg
         .guild_id
         .ok_or_else(|| anyhow!("Mensagem não foi enviada de uma guilda (Não deveria ocorrer)"))?;
 
-    let time_online_by_user = service.get_stats_of_guild(guild_id.0).await?;
+    let guild_stats = service.get_stats_of_guild(guild_id.0, target).await?;
 
-    let build_message_lines_tasks: Vec<_> = time_online_by_user
+    let build_message_lines_tasks: Vec<_> = guild_stats
+        .stats
         .into_iter()
         .map(|f| async move {
-            let user = guild_id.member(&ctx.http, f.0).await?;
+            let user = guild_id.member(&ctx.http, f.user_id).await?;
+            let seconds_online = f.seconds_online;
+            let hours_online = seconds_online / 60 / 60;
 
-            Ok(format!("- {} ficou {} segundos online ({} horas)", user.mention(), f.1, f.1 / 60 / 60))
+            Ok(format!(
+                "- {} ficou {} segundos online ({} horas)",
+                user.mention(),
+                seconds_online,
+                hours_online
+            ))
         })
         .collect();
 
     let lines = join_all(build_message_lines_tasks).await.log_errors();
 
     let mut message_builder = MessageBuilder::new();
+    message_builder.push_line(format!("Dados coletados desde: {}", guild_stats.initial_date));
 
     for line in lines {
         message_builder.push_line(line);
