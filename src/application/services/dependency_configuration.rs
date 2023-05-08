1dc32baf3c5958502a3944660060de2b324d2f3c
João Xavier <fastjonh@gmail.com>
2023-05-08 00:01:51 -0300

    migrated to poise dependency injection

--- src/application/services/dependency_configuration.rs
+++ src/application/services/dependency_configuration.rs
@@ -1,33 +1,47 @@
+use std::sync::Mutex;
+
+use anyhow::Error;
 use mongodb::Database;
-use serenity::prelude::TypeMap;
-use tokio::sync::RwLockWriteGuard;
 
 use crate::application::{
     models::{
-        allowed_ids::AllowedIds,
         appsettings::{AppConfigurations, AppSettings},
     },
     services::{
         command_services::CommandServices, guild_services::GuildServices,
         stats_services::StatsServices, user_services::UserServices,
-    },
+    }, infra::mongo_client::create_mongo_client,
 };
 
-pub fn register_dependencies(
-    mut data: RwLockWriteGuard<TypeMap>,
-    settings: AppSettings,
-    mongo_database: Database,
-) {
-    let guild_services = GuildServices::new(&mongo_database);
-    let user_services = UserServices::new(&mongo_database, guild_services.to_owned());
-    let command_services = CommandServices::new(&mongo_database, user_services.to_owned());
-    let stats_services = StatsServices::new(&mongo_database);
-    let app_configurations = AppConfigurations::new();
+pub struct DependencyContainer {
+    pub allowed_ids: Vec<u64>,
+    pub user_services: UserServices,
+    pub guild_services: GuildServices,
+    pub command_services: CommandServices,
+    pub stats_services: StatsServices,
+    pub app_configurations: Mutex<AppConfigurations>,
+}
 
-    data.insert::<AllowedIds>(settings.allowed_ids);
-    data.insert::<UserServices>(user_services);
-    data.insert::<GuildServices>(guild_services);
-    data.insert::<CommandServices>(command_services);
-    data.insert::<StatsServices>(stats_services);
-    data.insert::<AppConfigurations>(app_configurations);
+impl DependencyContainer {
+    fn new(db: Database, settings: AppSettings) -> Self {
+        let guild_services = GuildServices::new(&db);
+        let user_services = UserServices::new(&db, guild_services.to_owned());
+        let command_services = CommandServices::new(&db, user_services.to_owned());
+        let stats_services = StatsServices::new(&db);
+        let app_configurations = AppConfigurations::new();
+
+        Self {
+            allowed_ids: settings.allowed_ids,
+            user_services,
+            guild_services,
+            command_services,
+            stats_services,
+            app_configurations: Mutex::new(app_configurations),
+        }
+    }
+
+    pub async fn build(settings: AppSettings) -> Result<Self, Error> {
+        let db = create_mongo_client(&settings).await?.database("acn_r");
+        Ok(Self::new(db, settings))
+    }
 }
