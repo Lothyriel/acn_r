d51963ae213b3c8b91f04232975e003829048863
João Xavier <fastjonh@gmail.com>
2023-05-08 12:03:15 -0300

    migrando para poise (mitada jr do mutex e await, "cannot be sent between threads safely")

--- src/features/commands/misc/debug.rs
+++ src/features/commands/misc/debug.rs
@@ -1,35 +1,27 @@
 use anyhow::anyhow;
-use serenity::{
-    framework::standard::{macros::command, Args, CommandResult},
-    model::prelude::Message,
-    prelude::Context,
-};
+use poise::command;
 
-use crate::application::models::appsettings::AppConfigurations;
+use crate::extensions::serenity_ext::{CommandResult, Context};
 
-#[command]
-#[owners_only]
-#[description("Liga/Desliga o modo debug")]
-async fn debug(ctx: &Context, msg: &Message, _args: Args) -> CommandResult {
-    let mut data = ctx.data.write().await;
+#[command(prefix_command, slash_command, category = "Misc")]
+pub async fn debug(ctx: Context<'_>) -> CommandResult {
+    let option = {
+        let mut configurations = ctx
+            .data()
+            .app_configurations
+            .write()
+            .map_err(|_| anyhow!("Failed to get write lock on AppConfigurations"))?;
 
-    let configurations = data.get_mut::<AppConfigurations>().ok_or_else(|| {
-        anyhow!(
-            "Não tem {} cadastrado vei...",
-            stringify!($AppConfigurations)
-        )
-    })?;
+        configurations.debug = !configurations.debug;
 
-    configurations.debug = !configurations.debug;
-
-    let option = if configurations.debug {
-        "Ligado"
-    } else {
-        "Desligado"
+        if configurations.debug {
+            "Ligado"
+        } else {
+            "Desligado"
+        }
     };
 
-    msg.reply(&ctx.http, format!("O modo debug está {option}"))
-        .await?;
+    ctx.say(format!("O modo debug está {option}")).await?;
 
     Ok(())
 }
