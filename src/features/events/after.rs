450fe9a9557a054d8c6ef34509fd82c7758185cb
João Xavier <fastjonh@gmail.com>
2023-05-08 12:04:55 -0300

    -migrando para poise -extraindo funcoes

--- src/features/events/after.rs
+++ src/features/events/after.rs
@@ -1,92 +1,38 @@
 use anyhow::Error;
-use serenity::{
-    framework::standard::{macros::hook, CommandResult},
-    model::prelude::Message,
-    prelude::Context,
-};
 
 use crate::{
-    application::{
-        models::{
-            appsettings::AppConfigurations,
-            dto::{command_dto::CommandUseDto, user_services::GuildInfo},
-        },
-        services::command_services::CommandServices,
+    application::models::dto::command_dto::CommandUseDto,
+    extensions::{
+        log_ext::LogExt,
+        serenity_ext::{Context, ContextExt},
     },
-    extensions::{dependency_ext::Dependencies, log_ext::LogExt},
 };
 
-#[hook]
-pub async fn after(ctx: &Context, msg: &Message, name: &str, result: CommandResult) {
-    handler(ctx, msg, name, result).await.log();
-}
-
-async fn handler<'a>(
-    ctx: &'a Context,
-    msg: &'a Message,
-    name: &'a str,
-    result: CommandResult,
-) -> Result<(), Error> {
+async fn after(ctx: Context<'_>) -> Result<(), Error> {
     let now = chrono::Utc::now();
 
-    let command_services = ctx.get_dependency::<CommandServices>().await?;
+    let command_services = &ctx.data().command_services;
 
-    let guild_info = get_guild_info(msg, ctx);
+    let guild_info = ctx.get_guild_info();
 
-    let nickname = msg
-        .author_nick(&ctx.http)
-        .await
-        .unwrap_or_else(|| msg.author.name.to_string());
+    let nickname = ctx.get_author_name().await;
 
-    let command_with_prefix = format!("!{name}");
-    let args = msg
-        .content
-        .replace(&command_with_prefix, "")
-        .trim()
-        .to_string();
+    let command_name = ctx.command().name.to_string();
 
     let dto = CommandUseDto {
         date: now,
         guild_info,
-        user_id: msg.author.id.0,
+        user_id: ctx.author().id.0,
         user_nickname: nickname,
-        command: name.to_string(),
-        args,
+        command: command_name,
+        args: ctx.get_command_args().await,
     };
-
-    if let Err(error) = result {
-        let message = format!("{}: {}", msg.id, error);
-        println!("{message}");
-        discord_debug(ctx, msg, &message).await?;
-        command_services.add_command_error(&dto, message).await?;
-    }
 
     command_services.add_command_use(dto).await?;
 
     Ok(())
 }
 
-async fn discord_debug(ctx: &Context, msg: &Message, error: &str) -> Result<(), Error> {
-    let configuration = ctx.get_dependency::<AppConfigurations>().await?;
-
-    if configuration.debug {
-        msg.reply(&ctx.http, error).await?;
-    }
-
-    Ok(())
-}
-
-fn get_guild_info(msg: &Message, ctx: &Context) -> Option<GuildInfo> {
-    let guild_id = msg.guild_id.map(|g| g.0);
-    let guild_name = msg.guild(&ctx.cache).map(|g| g.name);
-
-    if let Some(id) = guild_id {
-        if let Some(name) = guild_name {
-            return Some(GuildInfo {
-                guild_id: id,
-                guild_name: name,
-            });
-        }
-    }
-    None
+pub async fn handler(ctx: Context<'_>) {
+    after(ctx).await.log();
 }
