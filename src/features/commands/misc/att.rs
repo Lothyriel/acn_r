e2163b07c0ecdab40424b10a6933234e702eb214
João Xavier <fastjonh@gmail.com>
2023-05-08 00:11:08 -0300

    migrated to poise

--- src/features/commands/misc/att.rs
+++ src/features/commands/misc/att.rs
@@ -1,30 +1,21 @@
-use anyhow::anyhow;
-use serenity::{
-    framework::standard::{macros::command, Args, CommandResult},
-    futures::future::join_all,
-    model::prelude::Message,
-    prelude::Context,
+use poise::command;
+use serenity::{futures::future::join_all, http::CacheHttp};
+
+use crate::extensions::{
+    log_ext::LogErrorsExt,
+    serenity_ext::{CommandResult, Context, GuildExt},
 };
 
-use crate::extensions::{guild_ext::GuildExt, log_ext::LogErrorsExt};
-
-#[command]
-#[owners_only]
-#[description("Manda uma mensagem em todos os grupos onde esse bot está presente")]
-async fn att(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
-    if args.is_empty() {
-        let message = format!("O comando precisa de uma mensagem como argumento");
-        msg.reply(&ctx.http, message).await?;
-        Err(anyhow!("Faltando Argumentos"))?;
-    }
-
-    let message = args.rest();
-
-    let guilds = ctx.http.get_guilds(None, None).await?;
+#[command(prefix_command, slash_command)]
+async fn att(
+    ctx: Context<'_>,
+    #[description = "Prompt to search for"] message: String,
+) -> CommandResult {
+    let guilds = ctx.http().get_guilds(None, None).await?;
 
     let tasks: Vec<_> = guilds
         .iter()
-        .map(|x| x.id.say_on_main_text_channel(&ctx.http, message))
+        .map(|x| x.id.say_on_main_text_channel(ctx.http(), &message))
         .collect();
 
     join_all(tasks).await.log_errors();
