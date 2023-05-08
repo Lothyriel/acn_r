8503a546636beda6911771c42cd2e0598ffbb031
João Xavier <fastjonh@gmail.com>
2023-05-08 12:00:59 -0300

    -adicionando types para classes comuns do poise -extraindo alguns metodos para extension methods

--- src/extensions/serenity_ext.rs
+++ src/extensions/serenity_ext.rs
@@ -1,7 +1,9 @@
 use anyhow::{anyhow, Error};
 use serenity::{async_trait, http::Http, model::prelude::GuildId};
 
-use crate::application::services::dependency_configuration::DependencyContainer;
+use crate::application::{
+    models::dto::user_services::GuildInfo, services::dependency_configuration::DependencyContainer,
+};
 
 #[async_trait]
 pub trait GuildExt {
@@ -25,7 +27,63 @@ impl GuildExt for GuildId {
     }
 }
 
-pub type Command = poise::Command<DependencyContainer, Error>;
+#[async_trait]
+pub trait ContextExt {
+    async fn discord_debug(self, error: &'_ str) -> Result<(), Error>;
+    async fn get_author_name(self) -> String;
+    async fn get_command_args(self) -> String;
+    fn get_guild_info(self) -> Option<GuildInfo>;
+}
+
+#[async_trait]
+impl ContextExt for Context<'_> {
+    async fn discord_debug(self, error: &'_ str) -> Result<(), Error> {
+        let debug = {
+            let configuration = self
+                .data()
+                .app_configurations
+                .read()
+                .map_err(|_| anyhow!("Failed to get read lock on AppConfigurations"))?;
+
+            configuration.debug
+        };
+
+        if debug {
+            self.say(error).await?;
+        }
+
+        Ok(())
+    }
+
+    async fn get_author_name(self) -> String {
+        self.author_member()
+            .await
+            .map(|a| a.display_name().to_string())
+            .unwrap_or_else(|| self.author().name.to_string())
+    }
+
+    async fn get_command_args(self) -> String {
+        todo!()
+    }
+
+    fn get_guild_info(self) -> Option<GuildInfo> {
+        let guild_id = self.guild_id().map(|g| g.0);
+        let guild_name = self.guild_id().and_then(|g| g.name(self));
+
+        if let Some(id) = guild_id {
+            if let Some(name) = guild_name {
+                return Some(GuildInfo {
+                    guild_id: id,
+                    guild_name: name,
+                });
+            }
+        }
+        None
+    }
+}
+
 pub type Context<'a> = poise::Context<'a, DependencyContainer, Error>;
-pub type FrameworkContext<'a> = poise::FrameworkContext<'a, DependencyContainer, Error>;
+pub type Command = poise::Command<DependencyContainer, Error>;
 pub type CommandResult = Result<(), Error>;
+pub type FrameworkContext<'a> = poise::FrameworkContext<'a, DependencyContainer, Error>;
+pub type FrameworkError<'a> = poise::FrameworkError<'a, DependencyContainer, Error>;
