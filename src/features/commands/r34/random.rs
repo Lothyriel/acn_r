b1ee14dc9910af24caae1dabbf343732d901bd76
João Xavier <fastjonh@gmail.com>
2023-05-08 12:03:49 -0300

    adicionando categoria

--- src/features/commands/r34/random.rs
+++ src/features/commands/r34/random.rs
@@ -2,10 +2,10 @@ use poise::command;
 
 use crate::extensions::serenity_ext::{CommandResult, Context};
 
-#[command(prefix_command, slash_command)]
+#[command(prefix_command, slash_command, category = "R34")]
 pub async fn random(
     _ctx: Context<'_>,
-    #[description = "Prompt to search for"] search: Option<String>,
+    #[description = "Prompt to search for"] _search: Option<String>,
 ) -> CommandResult {
     let _now = chrono::Utc::now();
 
