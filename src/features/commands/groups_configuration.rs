20570f5e333ff55c93cc5a1ef49251381c222d2d
João Xavier <fastjonh@gmail.com>
2023-05-08 12:01:44 -0300

    -adicionando grupo misc -adicionando help command

--- src/features/commands/groups_configuration.rs
+++ src/features/commands/groups_configuration.rs
@@ -1,14 +1,14 @@
 use crate::{
     extensions::serenity_ext::Command,
-    features::commands::{misc::misc_group::misc_group, r34::r34_group::r34_group},
+    features::commands::{help::help, misc::misc_group::misc_group, r34::r34_group::r34_group},
 };
 
 fn register_groups() -> Vec<Vec<Command>> {
-    vec![r34_group()]
+    vec![r34_group(), misc_group()]
 }
 
 pub fn register_commands() -> Vec<Command> {
-    let mut commands = Vec::new();
+    let mut commands = vec![help()];
     for mut command in register_groups() {
         commands.append(command.as_mut());
     }
