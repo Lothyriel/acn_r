25739df286432021d21b37d6ecc0d6c0794d4614
João Xavier <fastjonh@gmail.com>
2023-05-09 06:20:08 -0300

    added body to start_deploy method

--- src/application/services/github_services.rs
+++ src/application/services/github_services.rs
@@ -1,3 +1,5 @@
+use anyhow::Error;
+use log::info;
 use mongodb::Database;
 
 #[derive(Clone)]
@@ -6,5 +8,10 @@ pub struct GithubServices {}
 impl GithubServices {
     pub fn new(_database: &Database) -> Self {
         Self {}
+    }
+
+    pub async fn start_deploy(&self) -> Result<(), Error> {
+        info!("(TODO!) Should call Github API and trigger the action deploy");
+        Ok(())
     }
 }
