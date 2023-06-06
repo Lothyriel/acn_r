36c1068e755d37a28d1676d2cdfa68890ac937c2
João Xavier <fastjonh@gmail.com>
2023-06-06 20:20:37 -0300

    deriving debug

--- src/application/models/entities/user_activity.rs
+++ src/application/models/entities/user_activity.rs
@@ -2,7 +2,7 @@ use crate::application::models::entities::user::Activity;
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct UserActivity {
     pub guild_id: u64,
     pub user_id: u64,
