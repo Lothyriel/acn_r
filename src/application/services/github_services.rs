3d3911f1764c63170de80b3cc981bc9b8dda0cfd
João Xavier <fastjonh@gmail.com>
2023-06-07 02:25:42 -0300

    extracting methods to ext trait

--- src/application/services/github_services.rs
+++ src/application/services/github_services.rs
@@ -1,16 +1,17 @@
-use std::{sync::Arc, time::Duration};
-
-use anyhow::{anyhow, Error};
+use anyhow::Error;
 use log::warn;
-use poise::serenity_prelude::Guild;
 use serenity::{client::Cache, http::Http};
+use std::{sync::Arc, time::Duration};
 use tokio::{
     sync::{RwLock, Semaphore},
     time::sleep,
 };
 
-use crate::application::infra::{
-    appsettings::AppConfigurations, http_clients::github_client::GithubClient,
+use crate::{
+    application::infra::{
+        appsettings::AppConfigurations, http_clients::github_client::GithubClient,
+    },
+    extensions::serenity::guild_ext,
 };
 
 const SECONDS_IN_30_MINUTES: u64 = 30 * 60;
@@ -53,16 +54,14 @@ impl GithubServices {
     }
 
     async fn poll_deploy(&self, http: Arc<Http>, cache: Arc<Cache>) -> Result<(), Error> {
-        let someone_online = self
-            .is_someone_online(http.to_owned(), cache.to_owned())
-            .await?;
+        let someone_online = is_someone_online(http.to_owned(), cache.to_owned()).await?;
 
         match someone_online {
             true => Ok(()),
             false => {
                 warn!("Deploying in {SECONDS_IN_30_MINUTES} seconds");
                 sleep(Duration::from_secs(SECONDS_IN_30_MINUTES)).await;
-                match self.is_someone_online(http, cache).await? {
+                match is_someone_online(http, cache).await? {
                     true => {
                         warn!("Deploy cancelled");
                         Ok(())
@@ -76,37 +75,18 @@ impl GithubServices {
         }
     }
 
-    async fn is_someone_online(&self, http: Arc<Http>, cache: Arc<Cache>) -> Result<bool, Error> {
-        let guilds = get_guilds(&http, cache).await?;
-
-        let voice_states: Vec<_> = guilds
-            .into_iter()
-            .flat_map(|g| g.voice_states.into_values())
-            .collect();
-
-        let online_count = voice_states
-            .iter()
-            .filter(|p| p.channel_id.is_some())
-            .count();
-
-        warn!("Users online: {}", online_count);
-
-        Ok(online_count > usize::MIN)
-    }
-
     async fn start_deploy(&self) -> Result<(), Error> {
         warn!("Calling Github API and triggering action deploy");
         self.client.deploy().await
     }
 }
 
-async fn get_guilds(http: &Arc<Http>, cache: Arc<Cache>) -> Result<Vec<Guild>, Error> {
-    let guilds_info = http.get_guilds(None, None).await?;
+async fn is_someone_online(http: Arc<Http>, cache: Arc<Cache>) -> Result<bool, Error> {
+    let online_users = guild_ext::get_all_online_users(http, cache).await?;
 
-    let get_guild_results = guilds_info.into_iter().map(|g| {
-        g.id.to_guild_cached(&cache)
-            .ok_or_else(|| anyhow!("Couldn't get Guild {} from cache", g.id))
-    });
+    let count = online_users.len();
 
-    get_guild_results.collect()
+    warn!("Users online: {count}");
+
+    Ok(count > usize::MIN)
 }
--- src/extensions/serenity/guild_ext.rs
+++ src/extensions/serenity/guild_ext.rs
@@ -1,9 +1,14 @@
+use std::sync::Arc;
+
 use anyhow::{anyhow, Error};
-use serenity::{async_trait, http::Http, model::prelude::GuildId};
+use serenity::{async_trait, client::Cache, http::Http, model::prelude::GuildId};
+
+use crate::extensions::std_ext::VecResultErrorExt;
 
 #[async_trait]
 pub trait GuildExt {
     async fn say_on_main_text_channel(self, http: &Http, msg: &str) -> Result<(), Error>;
+    fn get_online_users(self, cache: Arc<Cache>) -> Result<Vec<u64>, Error>;
 }
 
 #[async_trait]
@@ -21,4 +26,32 @@ impl GuildExt for GuildId {
 
         Ok(())
     }
+
+    fn get_online_users(self, cache: Arc<Cache>) -> Result<Vec<u64>, Error> {
+        let guild = self
+            .to_guild_cached(&cache)
+            .ok_or_else(|| anyhow!("Couldn't get Guild {} from cache", self.0))?;
+
+        let online_users = guild
+            .voice_states
+            .into_values()
+            .filter(|v| v.channel_id.is_some())
+            .map(|c| c.user_id.0)
+            .collect();
+
+        Ok(online_users)
+    }
+}
+
+pub async fn get_all_online_users(http: Arc<Http>, cache: Arc<Cache>) -> Result<Vec<u64>, Error> {
+    let guilds_info = http.get_guilds(None, None).await?;
+
+    let get_online_users_results: Vec<_> = guilds_info
+        .into_iter()
+        .map(|g| g.id.get_online_users(cache.to_owned()))
+        .collect();
+
+    let all_online_users = get_online_users_results.all_successes()?;
+
+    Ok(all_online_users.into_iter().flatten().collect())
 }
