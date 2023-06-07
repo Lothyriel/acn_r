73bea9ed564e41c2915e20b5ac027868e1f158c2
João Xavier <fastjonh@gmail.com>
2023-06-06 23:12:04 -0300

    adding serde property to rename

--- src/application/models/entities/user_activity.rs
+++ src/application/models/entities/user_activity.rs
@@ -1,12 +1,14 @@
 use crate::application::models::entities::user::Activity;
 use chrono::{DateTime, Utc};
+use mongodb::bson::oid::ObjectId;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct UserActivity {
+    #[serde(rename = "_id")]
+    pub id: Option<ObjectId>,
     pub guild_id: u64,
     pub user_id: u64,
     pub date: DateTime<Utc>,
     pub activity_type: Activity,
-    pub id: Option<String>
 }
