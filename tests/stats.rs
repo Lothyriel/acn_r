e399ce10f304020c02e1c5cf031730faa367deec
João Xavier <fastjonh@gmail.com>
2023-06-07 03:01:36 -0300

    grouping stats tests

--- tests/stats.rs
+++ tests/stats.rs
@@ -1,15 +1,19 @@
 #[cfg(test)]
 mod stats {
+    use std::str::FromStr;
+
     use acn_r::{
         application::{
             dependency_configuration::DependencyContainer,
             models::{dto::user::UpdateActivityDto, entities::user::Activity},
-            services::user_services::UserServices,
+            services::{user_services::UserServices, stats_services::OnlineStatusProvider},
         },
         init_app,
     };
     use anyhow::{anyhow, Error};
     use chrono::Duration;
+    use mongodb::bson::oid::ObjectId;
+    use serenity::async_trait;
 
     const LA_PALOMBA_ID: u64 = 244922266050232321;
     const LOTHYRIEL_ID: u64 = 244922703667003392;
@@ -38,6 +42,32 @@ mod stats {
         assert_eq!(lothyriel_data.user_id, LOTHYRIEL_ID);
 
         Ok(())
+    }
+    
+    #[tokio::test]
+    async fn should_filter_activity_discrepancies() -> Result<(), Error> {
+        let settings = init_app()?;
+        let data = DependencyContainer::build(settings).await?;
+
+        let spoiled_id = ObjectId::from_str("647fbb6bba0e327540cfed7b")?;
+
+        let removed_ids = data
+            .stats_services
+            .clean_spoiled_stats(LA_PALOMBA_ID, MockStatusProvider)
+            .await?;
+
+        assert!(removed_ids[0] == spoiled_id);
+
+        Ok(())
+    }
+
+    struct MockStatusProvider;
+
+    #[async_trait]
+    impl OnlineStatusProvider for MockStatusProvider {
+        async fn get_status(&self) -> Result<Vec<u64>, Error> {
+            Ok(vec![LOTHYRIEL_ID])
+        }
     }
 
     async fn populate_test_stats(user_services: UserServices) -> Result<(), Error> {
