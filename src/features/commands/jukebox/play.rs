47287cf345867fc868917f31b960353b0165662d
João Xavier <fastjonh@gmail.com>
2023-06-09 07:38:55 -0300

    implementando comando play

---
+++ src/features/commands/jukebox/play.rs
@@ -0,0 +1,14 @@
+use poise::command;
+
+use crate::extensions::serenity::context_ext::ContextExt;
+use crate::extensions::serenity::serenity_structs::{CommandResult, Context};
+
+#[command(prefix_command, slash_command, track_edits, guild_only)]
+pub async fn play(
+    ctx: Context<'_>,
+    #[description = "A song URL or YouTube search query"] query: String,
+) -> CommandResult {
+    let songbird = ctx.get_songbird().await?;
+
+    songbird.queue(ctx, query).await
+}
