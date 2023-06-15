81a73479d6a52992b646d76d400238d01c348653
fastjonh <fastjonh@gmail.com>
2023-06-15 05:13:44 -0300

    -getting port from settings -added lavalink settings to struct -setting ports on environments

--- src/application/infra/songbird.rs
+++ src/application/infra/songbird.rs
@@ -28,7 +28,8 @@ pub async fn get_lavalink_client(
     let app_info = Http::new(token).get_current_application_info().await?;
 
     let lava_client = LavalinkClient::builder(app_info.id.0)
-        .set_host(&settings.lavalink_url)
+        .set_host(&settings.lavalink_settings.url)
+        .set_port(settings.lavalink_settings.port)
         .set_password(env::get("LAVALINK_PASSWORD")?)
         .build(LavalinkHandler)
         .await?;
