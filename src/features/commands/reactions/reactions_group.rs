fb60b5239a19be1e14d924a97c2322063dad0134
fastjonh <fastjonh@gmail.com>
2023-06-20 18:27:32 -0300

    deleting unnecessary file

--- src/features/commands/reactions/reactions_group.rs
+++
@@ -1,5 +0,0 @@
-use crate::{features::commands::reactions::{react::react, add_react::add_react}, extensions::serenity::serenity_structs::Command};
-
-pub fn reactions_group() -> Vec<Command> {
-    vec![react(), add_react()]
-}
