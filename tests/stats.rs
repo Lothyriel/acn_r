9a7dbed800b297ec2ed5338830307c22e522fa85
fastjonh <fastjonh@gmail.com>
2023-06-20 12:25:13 -0300

    removing clean spoiled stats tests

--- tests/stats.rs
+++ tests/stats.rs
@@ -5,8 +5,7 @@ mod stats {
             dependency_configuration::DependencyContainer,
             models::{dto::user::UpdateActivityDto, entities::user::Activity},
             services::{
-                guild_services::GuildServices,
-                stats_services::{OnlineStatusProvider, StatsServices},
+                guild_services::GuildServices, stats_services::StatsServices,
                 user_services::UserServices,
             },
         },
@@ -14,8 +13,7 @@ mod stats {
     };
     use anyhow::{anyhow, Error};
     use chrono::Duration;
-    use mongodb::{bson::oid::ObjectId, Database};
-    use poise::async_trait;
+    use mongodb::Database;
 
     const LA_PALOMBA_ID: u64 = 244922266050232321;
     const LOTHYRIEL_ID: u64 = 244922703667003392;
@@ -27,10 +25,10 @@ mod stats {
         let db = DependencyContainer::database(&settings).await?;
         let stats_services = StatsServices::new(&db);
 
-        populate_good_test_stats(&db).await?;
+        populate_test_stats(&db).await?;
 
         let guild_stats = stats_services
-            .get_guild_stats(LA_PALOMBA_ID, Some(LOTHYRIEL_ID), MockStatusProvider)
+            .get_guild_stats(LA_PALOMBA_ID, Some(LOTHYRIEL_ID))
             .await?;
 
         let lothyriel_data = guild_stats
@@ -46,34 +44,7 @@ mod stats {
         Ok(())
     }
 
-    #[tokio::test]
-    async fn should_filter_activity_discrepancies() -> Result<(), Error> {
-        let settings = init_app()?;
-        let db = DependencyContainer::database(&settings).await?;
-
-        let bad_activity = populate_bad_test_stats(&db).await?;
-
-        let stats_services = StatsServices::new(&db);
-
-        let removed_ids = stats_services
-            .clean_spoiled_stats(LA_PALOMBA_ID, MockStatusProvider)
-            .await?;
-
-        assert!(removed_ids.contains(&bad_activity));
-
-        Ok(())
-    }
-
-    struct MockStatusProvider;
-
-    #[async_trait]
-    impl OnlineStatusProvider for MockStatusProvider {
-        async fn get_status(&self) -> Result<Vec<u64>, Error> {
-            Ok(vec![])
-        }
-    }
-
-    async fn populate_good_test_stats(db: &Database) -> Result<(), Error> {
+    async fn populate_test_stats(db: &Database) -> Result<(), Error> {
         let user_services = UserServices::new(&db, GuildServices::new(&db));
 
         let mut date = chrono::Utc::now();
@@ -106,62 +77,5 @@ mod stats {
         }
 
         Ok(())
-    }
-
-    async fn populate_bad_test_stats(db: &Database) -> Result<ObjectId, Error> {
-        let user_services = UserServices::new(&db, GuildServices::new(&db));
-
-        let mut date = chrono::Utc::now();
-
-        for _ in 0..10 {
-            let connected = UpdateActivityDto {
-                user_id: LOTHYRIEL_ID,
-                guild_id: LA_PALOMBA_ID,
-                guild_name: "La Palombert".to_owned(),
-                nickname: "Lothyriel".to_owned(),
-                activity: Activity::Connected,
-                date,
-            };
-            user_services.update_user_activity(connected).await?;
-
-            date = date + Duration::hours(1);
-
-            let disconnected = UpdateActivityDto {
-                user_id: LOTHYRIEL_ID,
-                guild_id: LA_PALOMBA_ID,
-                guild_name: "La Palombert".to_owned(),
-                nickname: "Lothyriel".to_owned(),
-                activity: Activity::Disconnected,
-                date,
-            };
-            user_services.update_user_activity(disconnected).await?;
-
-            date = date + Duration::hours(1);
-        }
-
-        let wrong_disconnected = UpdateActivityDto {
-            user_id: LOTHYRIEL_ID,
-            guild_id: LA_PALOMBA_ID,
-            guild_name: "La Palombert".to_owned(),
-            nickname: "Lothyriel".to_owned(),
-            activity: Activity::Disconnected,
-            date,
-        };
-
-        let activity_id = user_services
-            .update_user_activity(wrong_disconnected)
-            .await?;
-
-        let connected = UpdateActivityDto {
-            user_id: LOTHYRIEL_ID,
-            guild_id: LA_PALOMBA_ID,
-            guild_name: "La Palombert".to_owned(),
-            nickname: "Lothyriel".to_owned(),
-            activity: Activity::Connected,
-            date,
-        };
-        user_services.update_user_activity(connected).await?;
-
-        Ok(activity_id)
     }
 }
