414061e4765c6e6fa918ceb30809f9463b6a643a
fastjonh <fastjonh@gmail.com>
2023-06-20 14:58:57 -0300

    rename

--- src/features/events/handlers/ready.rs
+++ src/features/events/handlers/ready.rs
@@ -12,10 +12,13 @@ pub async fn handler(
     container: &DependencyContainer,
     ready: &poise::serenity_prelude::Ready,
 ) -> Result<(), Error> {
-    let permitidos = &container.allowed_ids;
+    let allowed_ids = &container.services.allowed_ids;
+
     let message = format!("Estamos totalmente dentro! {}", ready.user.name);
+
     warn!("{message}");
-    let tasks = permitidos
+
+    let tasks = allowed_ids
         .into_iter()
         .map(|p| send_greetings(ctx, *p, &message));
 
@@ -26,6 +29,7 @@ pub async fn handler(
 
 async fn send_greetings(ctx: &Context, id: u64, message: &String) -> Result<(), Error> {
     let user = ctx.http.get_user(id).await?;
+    
     let channel = user.create_dm_channel(&ctx.http).await?;
 
     channel
