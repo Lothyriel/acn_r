cf7af7bb1119a0ca503a36e564dc25998f001ab7
fastjonh <fastjonh@gmail.com>
2023-06-20 19:14:33 -0300

    suiting updated models and dto on the repository

--- src/application/repositories/reaction.rs
+++ src/application/repositories/reaction.rs
@@ -1,30 +1,67 @@
 use anyhow::{anyhow, Error};
+use futures::{AsyncReadExt, AsyncWriteExt};
 use mongodb::{
-    bson::{doc, from_document},
-    Collection, Database,
+    bson::{doc, from_document, Bson},
+    options::GridFsBucketOptions,
+    Collection, Database, GridFsBucket,
 };
 
-use crate::application::models::entities::reaction::Reaction;
+use crate::application::models::{dto::reaction_dto::ReactionDto, entities::reaction::Reaction};
 
 #[derive(Clone)]
 pub struct ReactionRepository {
     reactions: Collection<Reaction>,
+    bucket: GridFsBucket,
 }
 
 impl ReactionRepository {
     pub fn new(db: &Database) -> Self {
+        let bucket_options = GridFsBucketOptions::builder()
+            .bucket_name("Reactions".to_owned())
+            .build();
+
         Self {
             reactions: db.collection("Reactions"),
+            bucket: db.gridfs_bucket(bucket_options),
         }
     }
 
-    pub async fn add_reaction(&self, reaction: Reaction) -> Result<(), Error> {
-        self.reactions.insert_one(reaction, None).await?;
+    pub async fn add_reaction(&self, dto: ReactionDto) -> Result<(), Error> {
+        let id = Bson::ObjectId(dto.reaction.file.id);
+
+        let mut stream =
+            self.bucket
+                .open_upload_stream_with_id(id, &dto.reaction.file.filename, None);
+
+        stream.write_all(&dto.bytes).await?;
+
+        self.reactions.insert_one(dto.reaction, None).await?;
 
         Ok(())
     }
 
-    pub async fn reaction(&self, emotion: String, guild: Option<u64>) -> Result<Reaction, Error> {
+    pub async fn reaction(
+        &self,
+        emotion: String,
+        guild: Option<u64>,
+    ) -> Result<ReactionDto, Error> {
+        let reaction = self.get_reaction(emotion, guild).await?;
+
+        let mut stream = self
+            .bucket
+            .open_download_stream(Bson::ObjectId(reaction.file.id))
+            .await?;
+
+        let mut buffer = vec![];
+        stream.read_to_end(&mut buffer).await?;
+
+        Ok(ReactionDto {
+            reaction,
+            bytes: buffer,
+        })
+    }
+
+    async fn get_reaction(&self, emotion: String, guild: Option<u64>) -> Result<Reaction, Error> {
         let pipeline = [
             doc! { "$match": {"emotion": emotion, "guild_id": guild.map(|x| x as i64)} },
             doc! { "$sample": { "size": 1 } },
