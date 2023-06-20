5a8a4ec923daf03d4f821995c9e323fdee449942
fastjonh <fastjonh@gmail.com>
2023-06-20 19:12:40 -0300

    added DbFile for GridFS abstraction

--- src/application/models/entities/reaction.rs
+++ src/application/models/entities/reaction.rs
@@ -1,10 +1,11 @@
 use serde::{Deserialize, Serialize};
 
+use crate::application::models::db_file::DbFile;
+
 #[derive(Serialize, Deserialize)]
 pub struct Reaction {
     pub emotion: String,
     pub guild_id: Option<u64>,
     pub user_id: u64,
-    pub file_name: String,
-    pub bytes: Vec<u8>,
+    pub file: DbFile,
 }
