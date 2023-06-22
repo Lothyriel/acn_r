7b515ff21bb0414052466bfbbf12154ed4767dee
fastjonh <fastjonh@gmail.com>
2023-06-22 04:33:57 -0300

    using extracted function

--- src/features/events/handlers/voice_updated/dispatches/songbird_reconnect.rs
+++ src/features/events/handlers/voice_updated/dispatches/songbird_reconnect.rs
@@ -3,7 +3,7 @@ use std::sync::Arc;
 use anyhow::Error;
 
 use crate::{
-    application::{infra::lavalink_ctx::LavalinkCtx, models::entities::user::Activity},
+    application::models::entities::user::Activity,
     features::events::handlers::voice_updated::DispatchData,
 };
 
@@ -25,16 +25,7 @@ async fn dispatch_songbird_reconnect(dispatch_data: Arc<DispatchData>) -> Result
         None => return Ok(()),
     };
 
-    let lava_client = dispatch_data.lava_client.to_owned();
-    let jukebox_repository = dispatch_data.jukebox_repository.to_owned();
-
-    let songbird_ctx = LavalinkCtx::new(
-        dispatch_data.guild_id,
-        dispatch_data.user_id.0,
-        dispatch_data.songbird.to_owned(),
-        lava_client,
-        jukebox_repository,
-    );
+    let songbird_ctx = dispatch_data.get_lavalink_ctx().await;
 
     let _ = songbird_ctx.join_voice_channel(channel).await;
     songbird_ctx.join_voice_channel(channel).await
