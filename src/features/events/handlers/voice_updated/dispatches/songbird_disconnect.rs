703ee70122a1a87be4547554b85d5c709d6831b7
fastjonh <fastjonh@gmail.com>
2023-06-24 11:32:22 -0300

    fixing disconnect dispatch

--- src/features/events/handlers/voice_updated/dispatches/songbird_disconnect.rs
+++ src/features/events/handlers/voice_updated/dispatches/songbird_disconnect.rs
@@ -16,11 +16,23 @@ pub async fn handler(dispatch_data: Arc<DispatchData>) -> Result<(), Error> {
         return Ok(());
     }
 
-    let online_users = dispatch_data
+    let voice_states = dispatch_data
         .guild_id
-        .get_online_users(dispatch_data.cache.to_owned())?;
+        .get_voice_states(dispatch_data.cache.to_owned())?;
 
-    if online_users.is_empty() {
+    let state = match voice_states.get(&dispatch_data.user_id) {
+        Some(v) => v,
+        None => return Ok(()),
+    };
+
+    let channel = match state.channel_id {
+        Some(c) => c,
+        None => return Ok(()),
+    };
+
+    let someone_online = voice_states.values().any(|v| v.channel_id == Some(channel));
+
+    if !someone_online {
         let lava = dispatch_data.get_lavalink_ctx().await;
         lava.stop_player().await?;
     }
