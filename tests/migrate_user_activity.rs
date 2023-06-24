113da0ceefd8846890844fb7ddefcb93dea9041e
fastjonh <fastjonh@gmail.com>
2023-06-24 08:21:17 -0300

    updating migration function to work with json export files from mongodb

--- tests/migrate_user_activity.rs
+++ tests/migrate_user_activity.rs
@@ -2,65 +2,60 @@
 mod migrate {
     use acn_r::{
         application::{
-            infra::{env, mongo_client::create_mongo_client},
-            models::entities::user_activity::UserActivity,
+            infra::mongo_client::create_mongo_client,
+            models::entities::{user::Activity, user_activity::UserActivity},
         },
         init_app,
     };
     use anyhow::Error;
-    use reqwest::Client;
+    use chrono::Utc;
     use serde::Deserialize;
 
     #[tokio::test]
     async fn should_migrate_user_activity() -> Result<(), Error> {
         let settings = init_app()?;
 
-        let _collection = create_mongo_client(&settings.mongo_settings)
+        let collection = create_mongo_client(&settings.mongo_settings)
             .await?
             .database("acn_r")
             .collection::<UserActivity>("UserActivity");
 
-        // let data = get_data().await?;
+        let data = get_data().await?;
 
-        // collection.insert_many(data, None).await?;
+        collection.insert_many(data, None).await?;
 
         Ok(())
     }
 
-    async fn _get_data() -> Result<Vec<UserActivity>, Error> {
-        let client = Client::new();
+    async fn get_data() -> Result<Vec<UserActivity>, Error> {
+        let filename = "acn_r.UserActivity.json";
 
-        let url = "https://sa-east-1.aws.data.mongodb-api.com/app/data-bsxri/endpoint/data/v1/action/find";
-
-        let body = serde_json::json!({
-            "collection":"UserActivity",
-            "database":"acn_r",
-            "dataSource":"acn-cluster",
-            "limit": 50000,
-            "filter": {"activity_type": {
-                "$in": [
-                  "Connected",
-                  "Disconnected"
-                ]
-              }}
-        });
-
-        let response = client
-            .post(url)
-            .header("api-key", env::get("MIGRATION_API_KEY")?)
-            .json(&body)
-            .send()
-            .await?;
-
-        response.error_for_status_ref()?;
+        let settings_path = std::fs::read_to_string(filename)?;
 
         #[derive(Deserialize)]
-        struct Output {
-            _documents: Vec<UserActivity>,
+        struct Long {
+            #[serde(rename = "$numberLong")]
+            number_long: String,
         }
 
-        let output: Output = response.json().await?;
+        #[derive(Deserialize)]
+        struct TempActivity {
+            guild_id: Long,
+            user_id: Long,
+            date: chrono::DateTime<Utc>,
+            activity_type: Activity,
+        }
 
-        Ok(output._documents)
+        let activities: Vec<TempActivity> = serde_json::from_str(&settings_path)?;
+
+        Ok(activities
+            .into_iter()
+            .map(|a| UserActivity {
+                guild_id: a.guild_id.number_long.parse().unwrap(),
+                user_id: a.user_id.number_long.parse().unwrap(),
+                date: a.date,
+                activity_type: a.activity_type,
+            })
+            .collect())
     }
 }
