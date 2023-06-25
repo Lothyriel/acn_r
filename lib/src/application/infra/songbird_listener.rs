84481e663905d0ef6cdd696d8f6e9e8ad41912bc
fastjonh <fastjonh@gmail.com>
2023-06-25 09:44:57 -0300

    adding packets handlers

--- lib/src/application/infra/songbird_listener.rs
+++ lib/src/application/infra/songbird_listener.rs
@@ -1,22 +1,38 @@
-use std::sync::Arc;
-
 use anyhow::{anyhow, Error};
 use chrono::{DateTime, Utc};
-use dashmap::DashMap;
+use dashmap::{
+    mapref::one::{Ref, RefMut},
+    DashMap,
+};
 use lavalink_rs::async_trait;
-use songbird::{Event, EventContext, EventHandler};
+use log::warn;
+use songbird::{
+    events::context_data::{SpeakingUpdateData, VoiceData},
+    model::{
+        id::UserId,
+        payload::{ClientDisconnect, Speaking},
+    },
+    Event, EventContext, EventHandler,
+};
+use std::sync::Arc;
 
-use crate::{application::repositories::voice::VoiceRepository, extensions::log_ext::LogExt};
+use crate::{
+    application::{models::entities::voice::VoiceSnippet, repositories::voice::VoiceRepository},
+    extensions::log_ext::LogExt,
+};
 
 struct Snippet {
-    bytes: Vec<u8>,
+    bytes: Vec<i16>,
     date: DateTime<Utc>,
+    mapping: Option<UserId>,
 }
 
 pub struct VoiceController {
-    accumulator: DashMap<u64, Snippet>,
+    accumulator: DashMap<u32, Snippet>,
     repository: VoiceRepository,
 }
+
+const ONE_HUNDRED_KILOBYTES: usize = 100_000;
 
 impl VoiceController {
     pub fn new(repository: VoiceRepository) -> Self {
@@ -25,71 +41,161 @@ impl VoiceController {
             repository,
         }
     }
+
+    async fn handle_speaking_update(
+        &self,
+        data: &SpeakingUpdateData,
+        guild_id: u64,
+    ) -> Result<(), Error> {
+        if data.speaking {
+            return Ok(());
+        }
+
+        let snippet = self.get_snippet(data.ssrc)?;
+
+        if snippet.bytes.len() >= ONE_HUNDRED_KILOBYTES {
+            let id = snippet
+                .mapping
+                .ok_or_else(|| anyhow!("Buffer overflow without mapping"))?;
+
+            self.flush(data.ssrc, id, guild_id).await?;
+        }
+
+        Ok(())
+    }
+
+    fn get_snippet(&self, key: u32) -> Result<Ref<'_, u32, Snippet>, Error> {
+        self.accumulator
+            .get(&key)
+            .ok_or_else(|| anyhow!("Couldn't get Snippet after SpeakingUpdate"))
+    }
+
+    fn get_mut_snippet(&self, key: u32) -> Result<RefMut<'_, u32, Snippet>, Error> {
+        self.accumulator
+            .get_mut(&key)
+            .ok_or_else(|| anyhow!("Couldn't get Snippet after SpeakingUpdate"))
+    }
+
+    fn handle_speaking_state_update(&self, data: &Speaking) -> Result<(), Error> {
+        match data.user_id {
+            Some(_) => {
+                let mut snippet = self.get_mut_snippet(data.ssrc)?;
+                snippet.mapping = data.user_id;
+            }
+            None => {
+                warn!("Isso acontece muito?")
+            }
+        }
+
+        Ok(())
+    }
+
+    async fn handle_client_disconnect(
+        &self,
+        data: &ClientDisconnect,
+        guild_id: u64,
+    ) -> Result<(), Error> {
+        let snippet = self
+            .accumulator
+            .iter()
+            .filter(|a| a.mapping == Some(data.user_id))
+            .next()
+            .ok_or_else(|| anyhow!("WE NEED TO FIND THIS HERE"))?;
+
+        self.flush(*snippet.key(), data.user_id, guild_id).await
+    }
+
+    fn handle_voice_packet(&self, data: &VoiceData<'_>) -> Result<(), Error> {
+        let audio = data
+            .audio
+            .as_ref()
+            .ok_or_else(|| anyhow!("Could not decode packet"))?;
+
+        let key = data.packet.ssrc;
+
+        match self.accumulator.get_mut(&key) {
+            Some(mut m) => m.bytes.append(&mut audio.to_owned()),
+            None => {
+                let snippet = Snippet {
+                    bytes: audio.to_owned(),
+                    date: chrono::Utc::now(),
+                    mapping: None,
+                };
+
+                self.accumulator.insert(key, snippet);
+            }
+        }
+
+        Ok(())
+    }
+
+    async fn flush(&self, key: u32, user_id: UserId, guild_id: u64) -> Result<(), Error> {
+        let snippet = match self.accumulator.get(&key) {
+            Some(r) => r,
+            None => {
+                warn!("Usuário {user_id} desconectou sem nunca falar nada");
+                return Ok(());
+            }
+        };
+
+        {
+            self.accumulator.remove(&key);
+        }
+
+        let snippet = VoiceSnippet {
+            bytes: snippet.bytes.to_owned(),
+            date: snippet.date,
+            user_id: user_id.0,
+            guild_id,
+        };
+
+        self.repository.add_voice_snippet(snippet).await
+    }
 }
 
 pub struct Receiver {
     controller: Arc<VoiceController>,
+    guild_id: u64,
 }
 
 impl Receiver {
-    pub fn new(controller: Arc<VoiceController>) -> Self {
-        Self { controller }
+    pub fn new(controller: Arc<VoiceController>, guild_id: u64) -> Self {
+        Self {
+            controller,
+            guild_id,
+        }
     }
 }
 
 #[async_trait]
 impl EventHandler for Receiver {
     async fn act(&self, ctx: &EventContext<'_>) -> Option<Event> {
-        handler(ctx).await.log();
+        handler(self.guild_id, ctx, self.controller.to_owned())
+            .await
+            .log();
 
         None
     }
 }
 
-async fn handler(ctx: &EventContext<'_>) -> Result<(), Error> {
+async fn handler(
+    guild_id: u64,
+    ctx: &EventContext<'_>,
+    controller: Arc<VoiceController>,
+) -> Result<(), Error> {
     match ctx {
-        // Speaking state update, typically describing how another voice
-        // user is transmitting audio data. Clients must send at least one such
-        // packet to allow SSRC/UserID matching.
-        EventContext::SpeakingStateUpdate(data) => {
-            println!(
-                "Speaking state update: user {:?} has SSRC {:?}, using {:?}",
-                data.user_id, data.ssrc, data.speaking,
-            );
-        }
-        // Speaking state transition, describing whether a given source has started/stopped
-        // transmitting. This fires in response to a silent burst, or the first packet
-        // breaking such a burst.
+        EventContext::SpeakingStateUpdate(data) => controller.handle_speaking_state_update(data),
+        EventContext::VoicePacket(data) => controller.handle_voice_packet(data),
         EventContext::SpeakingUpdate(data) => {
-            println!(
-                "Source {} has {} speaking.",
-                data.ssrc,
-                if data.speaking { "started" } else { "stopped" },
-            );
-        }
-        EventContext::VoicePacket(data) => {
-            let audio = data
-                .audio
-                .as_ref()
-                .ok_or_else(|| anyhow!("Could not decode packet"))?;
-
-            println!(
-                "Audio packet sequence {:05} has {:04} bytes (decompressed from {}), SSRC {}",
-                data.packet.sequence.0,
-                audio.len() * std::mem::size_of::<i16>(),
-                data.packet.payload.len(),
-                data.packet.ssrc,
-            );
+            controller.handle_speaking_update(data, guild_id).await
         }
         EventContext::ClientDisconnect(disconnect) => {
-            println!("Client disconnected: user {:?}", disconnect.user_id)
+            controller
+                .handle_client_disconnect(disconnect, guild_id)
+                .await
         }
-        _ => {
-            return Err(anyhow!(
-                "This handler shoudn't be subscribed to other events"
-            ))
-        }
+        _ => Err(anyhow!(
+            "This handler shoudn't be subscribed to other events"
+        )),
     }
-
-    Ok(())
 }
