f642cfbbee0554baee6f24f4eea5611daeadc2e1
fastjonh <fastjonh@gmail.com>
2023-06-25 05:38:06 -0300

    using simpler method so isnt neede to import the log ext

--- bots/listen_r/src/main.rs
+++ bots/listen_r/src/main.rs
@@ -1,6 +1,4 @@
-use lib::extensions::log_ext::LogExt;
-
 #[tokio::main]
 async fn main() {
-    lib::start_listener().await.log()
+    lib::configurations::start_listener()
 }
--- lib/src/configurations/mod.rs
+++ lib/src/configurations/mod.rs
@@ -1,2 +1,12 @@
-pub mod acn;
-pub mod listener;
+use crate::extensions::log_ext::LogExt;
+
+mod acn;
+mod listener;
+
+pub async fn start_acn() {
+    acn::start_acn().await.log();
+}
+
+pub async fn start_listener() {
+    listener::start_listener().await.log();
+}
