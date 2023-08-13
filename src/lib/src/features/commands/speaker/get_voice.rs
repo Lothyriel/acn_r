d7d683f59db9bfa2d8ce4f83fca934f9e7230056
fastjonh <fastjonh@gmail.com>
2023-08-13 19:25:16 -0300

    finishing command

--- src/lib/src/features/commands/speaker/get_voice.rs
+++ src/lib/src/features/commands/speaker/get_voice.rs
@@ -3,16 +3,18 @@ use poise::{
     serenity_prelude::{AttachmentType, Mentionable, UserId},
 };
 
-use crate::extensions::serenity::{CommandResult, Context};
+use crate::extensions::serenity::{context_ext::ContextExt, CommandResult, Context};
 
-#[command(prefix_command, slash_command, category = "R34")]
+#[command(prefix_command, slash_command, guild_only, category = "Speaker")]
 pub async fn get_voice(
     ctx: Context<'_>,
     #[description = "User to get voice"] _user: Option<UserId>,
 ) -> CommandResult {
     let voice = &ctx.data().repositories.voice;
 
-    let snippet = match voice.get_voice_snippet().await? {
+    let guild = ctx.assure_guild_context()?;
+
+    let snippet = match voice.get_voice_snippet(guild.0).await? {
         Some(s) => s,
         None => {
             ctx.say("Tem nada não...").await?;
@@ -20,16 +22,21 @@ pub async fn get_voice(
         }
     };
 
-    let mention = UserId(snippet.user_id).mention();
+    let user_id = ctx.get_user(snippet.user_id).await?;
 
     let file = AttachmentType::Bytes {
         data: snippet.voice_data.bytes.into(),
-        filename: format!("{}_{}", mention, snippet.date),
+        filename: format!("{}_{}.wav", user_id.name, snippet.date.timestamp()),
     };
 
+    let time_zone = ctx.get_time_zone();
+
     ctx.send(|x| {
-        x.attachment(file)
-            .content(format!("{} : {}", mention, snippet.date))
+        x.attachment(file).content(format!(
+            "{} : {}",
+            user_id.mention(),
+            snippet.date.with_timezone(&time_zone).date_naive()
+        ))
     })
     .await?;
 
