9ba7b8d26737d17a497251ddc44dce49f255e18f
fastjonh <fastjonh@gmail.com>
2023-08-13 16:26:43 -0300

    added comamnd to add signatures

---
+++ src/lib/src/features/commands/misc/signature.rs
@@ -0,0 +1,27 @@
+use poise::command;
+
+use crate::{
+    application::models::entities::user::Signature,
+    extensions::serenity::{CommandResult, Context},
+};
+
+#[command(prefix_command, slash_command, category = "Misc")]
+pub async fn set_signature(
+    ctx: Context<'_>,
+    #[description = "Emojis para assinatura"] emojis: String,
+) -> CommandResult {
+    let user = &ctx.data().repositories.user;
+
+    let signature = Signature {
+        date: chrono::Utc::now(),
+        user_id: ctx.author().id.0,
+        emojis: emojis.to_owned(),
+    };
+
+    user.add_signature(signature).await?;
+
+    ctx.say(format!("Assinatura adicionada: \"{}\"", emojis))
+        .await?;
+
+    Ok(())
+}
