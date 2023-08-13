db3439b4e131b5bbc1ef8c19f460a405f34546f7
fastjonh <fastjonh@gmail.com>
2023-08-13 16:26:57 -0300

    refac signatures to be configured by the user

--- src/lib/src/features/events/handlers/message.rs
+++ src/lib/src/features/events/handlers/message.rs
@@ -3,33 +3,25 @@ use poise::serenity_prelude::{Context, Message, ReactionType};
 
 use crate::application::dependency_configuration::DependencyContainer;
 
-const REACTIONS_UNICODE: [&str; 8] = ["🇨", "🇺", "♥️", "🇵", "🇷", "🇪", "🇹", "🅾️"];
-const ROLE_NAME: &str = "cu-preto";
-
 pub async fn handler(
     ctx: &Context,
     data: &DependencyContainer,
     message: &Message,
 ) -> Result<(), Error> {
-    let guild_id = match message.guild_id {
-        Some(g) => g,
-        None => return Ok(()),
-    };
+    let signature = data.repositories.user.get_last_signature(message.author.id.0).await?;
 
-    let roles = guild_id.roles(ctx).await?;
+    match signature {
+        Some(s) => react(message, ctx, &s.emojis).await?,
+        None => (),
+    }
 
-    let role = match roles.into_values().find(|r| r.name == ROLE_NAME) {
-        Some(r) => r,
-        None => return Ok(()),
-    };
+    Ok(())
+}
 
-    let has_role = message.author.has_role(&ctx.http, guild_id, role).await?;
-
-    if has_role {
-        for unicode_reaction in REACTIONS_UNICODE {
-            let reaction = ReactionType::Unicode(unicode_reaction.to_owned());
-            message.react(ctx, reaction).await?;
-        }
+async fn react(message: &Message, ctx: &Context, emojis: &str) -> Result<(), Error> {
+    for unicode_reaction in emojis.chars() {
+        let reaction = ReactionType::Unicode(unicode_reaction.to_string());
+        message.react(ctx, reaction).await?;
     }
 
     Ok(())
