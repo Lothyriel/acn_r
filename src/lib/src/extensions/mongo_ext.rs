9670517f0c03c2b43a774a8c85e3af3901963f00
fastjonh <fastjonh@gmail.com>
2023-08-14 16:13:56 -0300

    added mongo_ext

---
+++ src/lib/src/extensions/mongo_ext.rs
@@ -0,0 +1,36 @@
+use anyhow::{anyhow, Error};
+use futures::TryStreamExt;
+use mongodb::{
+    bson::{doc, from_document, Document},
+    Collection,
+};
+use poise::async_trait;
+use serde::de::DeserializeOwned;
+
+use super::std_ext::VecResultErrorExt;
+
+#[async_trait]
+pub trait CollectionExt<T> {
+    async fn random_sample(self, size: u32, filter: Document) -> Result<Vec<T>, Error>;
+}
+
+#[async_trait]
+impl<T: DeserializeOwned + Send + Sync> CollectionExt<T> for Collection<T> {
+    async fn random_sample(self, size: u32, filter: Document) -> Result<Vec<T>, Error> {
+        let pipeline = [
+            doc! { "$match": filter, },
+            doc! { "$sample": { "size": size } },
+        ];
+
+        let cursor = self.aggregate(pipeline, None).await?;
+
+        let documents: Vec<_> = cursor.try_collect().await?;
+
+        let entities: Vec<_> = documents
+            .into_iter()
+            .map(|d| from_document(d).map_err(|e| anyhow!(e)))
+            .collect();
+
+        entities.all_successes()
+    }
+}
