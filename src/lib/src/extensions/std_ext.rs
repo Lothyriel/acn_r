a6ce2d28d3ce06a3d80c1aa6f812d35c29bdc4ed
fastjonh <fastjonh@gmail.com>
2023-08-26 05:30:22 -0300

    refatoration commit zxzxxzzz

--- src/lib/src/extensions/std_ext.rs
+++ src/lib/src/extensions/std_ext.rs
@@ -1,33 +1,20 @@
 use anyhow::{anyhow, Error};
 
-pub trait VecResultExt<T, E> {
-    fn partition_results(self) -> (Vec<T>, Vec<E>);
-}
+pub fn join_errors<T, I>(input: I) -> Result<impl Iterator<Item = T>, Error>
+where
+    I: IntoIterator<Item = Result<T, Error>>,
+{
+    let mut iter = input.into_iter();
 
-impl<T, E> VecResultExt<T, E> for Vec<Result<T, E>> {
-    fn partition_results(self) -> (Vec<T>, Vec<E>) {
-        let (success, errors): (Vec<_>, Vec<_>) = self.into_iter().partition(Result::is_ok);
-        (
-            success.into_iter().filter_map(Result::ok).collect(),
-            errors.into_iter().filter_map(Result::err).collect(),
-        )
-    }
-}
+    match iter.any(|r| r.is_err()) {
+        false => Ok(iter.filter_map(Result::ok)),
+        true => {
+            let error_messages: Vec<_> = iter
+                .filter_map(Result::err)
+                .map(|e| format!("{e}"))
+                .collect();
 
-pub trait VecResultErrorExt<T> {
-    fn all_successes(self) -> Result<Vec<T>, Error>;
-}
-
-impl<T> VecResultErrorExt<T> for Vec<Result<T, Error>> {
-    fn all_successes(self) -> Result<Vec<T>, Error> {
-        let (success, errors) = self.partition_results();
-
-        match errors.len() > 1 {
-            false => Ok(success),
-            true => {
-                let error_messages: Vec<_> = errors.iter().map(|e| format!("{e}")).collect();
-                Err(anyhow!("Errors: {}", error_messages.join(" | ")))
-            }
+            Err(anyhow!("Errors: {}", error_messages.join(" | ")))
         }
     }
 }
