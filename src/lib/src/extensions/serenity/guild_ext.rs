e7fe45365bb645efc67d4396913f9e2afab1908c
fastjonh <fastjonh@gmail.com>
2023-08-26 21:29:36 -0300

    no allocations gang

--- src/lib/src/extensions/serenity/guild_ext.rs
+++ src/lib/src/extensions/serenity/guild_ext.rs
@@ -1,7 +1,4 @@
-use std::{
-    collections::{HashMap, HashSet},
-    sync::Arc,
-};
+use std::{collections::HashMap, sync::Arc};
 
 use anyhow::{anyhow, Error};
 use poise::{
@@ -14,7 +11,6 @@ use crate::extensions::std_ext::join_errors;
 #[async_trait]
 pub trait GuildExt {
     async fn say_on_main_text_channel(self, http: &Http, msg: &str) -> Result<(), Error>;
-    fn get_online_users(self, cache: Arc<Cache>) -> Result<HashSet<StatusInfo>, Error>;
     fn get_voice_states(self, cache: Arc<Cache>) -> Result<HashMap<UserId, VoiceState>, Error>;
 }
 
@@ -34,18 +30,6 @@ impl GuildExt for GuildId {
         Ok(())
     }
 
-    fn get_online_users(self, cache: Arc<Cache>) -> Result<HashSet<StatusInfo>, Error> {
-        let voice_states = self.get_voice_states(cache)?;
-
-        let online_users = voice_states
-            .into_values()
-            .filter(|v| v.channel_id.is_some())
-            .map(|c| StatusInfo::new(c.user_id.0, self.0))
-            .collect();
-
-        Ok(online_users)
-    }
-
     fn get_voice_states(self, cache: Arc<Cache>) -> Result<HashMap<UserId, VoiceState>, Error> {
         let guild = cache
             .guild(self)
@@ -58,16 +42,23 @@ impl GuildExt for GuildId {
 pub async fn get_all_online_users(
     http: Arc<Http>,
     cache: Arc<Cache>,
-) -> Result<HashSet<StatusInfo>, Error> {
+) -> Result<impl Iterator<Item = StatusInfo>, Error> {
     let guilds_info = http.get_guilds(None, None).await?;
 
-    let get_online_users_results = guilds_info
-        .into_iter()
-        .map(|g| g.id.get_online_users(cache.to_owned()));
+    let get_online_users_results = guilds_info.into_iter().map(move |g| {
+        let voice_states = g.id.get_voice_states(cache.to_owned())?;
+
+        let online_users = voice_states
+            .into_values()
+            .filter(|v| v.channel_id.is_some())
+            .map(move |c| StatusInfo::new(c.user_id.0, g.id.0));
+
+        Ok(online_users)
+    });
 
     let all_online_users = join_errors(get_online_users_results)?;
 
-    Ok(all_online_users.flatten().collect())
+    Ok(all_online_users.flatten())
 }
 
 #[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
