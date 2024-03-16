da04cab2a0359e321d21bb37e643d66cf2cc4e07
João Xavier <fastjonh@gmail.com>
2024-03-16 20:04:47 -0300

    updating to new poise way of things and cleaning up the builder objects

--- src/features/mod.rs
+++ src/features/mod.rs
@@ -1,6 +1,6 @@
 use anyhow::Error;
-use poise::serenity_prelude::GatewayIntents;
-use songbird::{Config, SerenityInit};
+use poise::serenity_prelude::{ClientBuilder, GatewayIntents};
+use songbird::SerenityInit;
 
 use crate::{
     application::{
@@ -34,25 +34,16 @@ pub async fn start() -> Result<(), Error> {
     let settings = AppSettings::load()?;
     let token = env::get("TOKEN_BOT")?;
 
-    let framework = poise::Framework::builder()
-        .options(poise::FrameworkOptions {
-            event_handler: |ctx, event, _, user_data| {
-                Box::pin(handlers::invoker::songbird_handler(ctx, event, user_data))
-            },
-            commands: register_commands(register_groups()),
-            on_error: |error| Box::pin(error::handler(error)),
-            post_command: |ctx| Box::pin(after::handler(ctx)),
-            prefix_options: poise::PrefixFrameworkOptions {
-                prefix: Some(settings.prefix.to_owned()),
-                mention_as_prefix: true,
-                ..Default::default()
-            },
-            command_check: Some(|ctx| Box::pin(check::handler(ctx))),
-            ..Default::default()
-        })
-        .token(&token)
-        .client_settings(|c| c.register_songbird_from_config(Config::default()))
-        .intents(GatewayIntents::all())
+    ClientBuilder::new(token, GatewayIntents::all())
+        .register_songbird()
+        .framework(get_framework(&settings))
+        .await?
+        .start()
+}
+
+fn get_framework(settings: &AppSettings) -> poise::FrameworkBuilder<DependencyContainer, Error> {
+    poise::Framework::builder()
+        .options(get_options(settings))
         .setup(|ctx, ready, framework| {
             Box::pin(async move {
                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
@@ -61,9 +52,23 @@ pub async fn start() -> Result<(), Error> {
 
                 DependencyContainer::build(settings, songbird, ready.user.id).await
             })
-        });
+        })
+}
 
-    framework.run().await?;
-
-    Ok(())
+fn get_options(settings: &AppSettings) -> poise::FrameworkOptions<DependencyContainer, Error> {
+    poise::FrameworkOptions {
+        event_handler: |ctx, event, _, user_data| {
+            Box::pin(handlers::invoker::songbird_handler(ctx, event, user_data))
+        },
+        commands: register_commands(register_groups()),
+        on_error: |error| Box::pin(error::handler(error)),
+        post_command: |ctx| Box::pin(after::handler(ctx)),
+        prefix_options: poise::PrefixFrameworkOptions {
+            prefix: Some(settings.prefix.to_owned()),
+            mention_as_prefix: true,
+            ..Default::default()
+        },
+        command_check: Some(|ctx| Box::pin(check::handler(ctx))),
+        ..Default::default()
+    }
 }
