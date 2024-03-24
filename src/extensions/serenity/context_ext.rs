4199b40a6a08df9d6c79a1fdbd7a7fb2005b6991
João Xavier <fastjonh@gmail.com>
2024-03-23 22:46:13 -0300

    fixing extension methods

--- src/extensions/serenity/context_ext.rs
+++ src/extensions/serenity/context_ext.rs
@@ -1,10 +1,11 @@
 use std::sync::Arc;
 
-use anyhow::{anyhow, Error};
+use anyhow::{anyhow, Result};
 use poise::async_trait;
-use poise::serenity_prelude::{ChannelId, Guild, GuildId, User};
+use poise::serenity_prelude::{ChannelId, GuildId, GuildRef, User, UserId};
 use songbird::Songbird;
 
+use crate::application::infra::audio::manager::AudioManager;
 use crate::{
     application::{infra::audio::player::AudioPlayer, models::dto::user::GuildInfo},
     extensions::{serenity::Context, std_ext::JoinString},
@@ -12,57 +13,53 @@ use crate::{
 
 #[async_trait]
 pub trait ContextExt {
-    async fn get_author_name(self) -> String;
-    async fn get_command_args(self) -> String;
-    async fn get_player(self) -> Result<AudioPlayer, Error>;
-    async fn assure_connected(self) -> Result<Option<ChannelId>, Error>;
-    async fn get_user(self, user_id: u64) -> Result<User, Error>;
-    fn get_guild_info(self) -> Option<GuildInfo>;
-    fn assure_cached_guild(self) -> Result<Guild, Error>;
-    fn assure_guild_context(self) -> Result<GuildId, Error>;
+    async fn get_author_name(&self) -> String;
+    async fn get_command_args(&self) -> String;
+    async fn get_player(&self) -> Result<AudioPlayer>;
+    async fn assure_connected(&self) -> Result<Option<ChannelId>>;
+    async fn get_user(&self, user_id: UserId) -> Result<User>;
+    fn get_guild_info(&self) -> Option<GuildInfo>;
+    fn assure_cached_guild(&self) -> Result<GuildRef<'_>>;
+    fn assure_guild_context(&self) -> Result<GuildId>;
 }
 
 #[async_trait]
 impl ContextExt for Context<'_> {
-    async fn get_author_name(self) -> String {
+    async fn get_author_name(&self) -> String {
         self.author_member()
             .await
             .map(|a| a.display_name().to_string())
             .unwrap_or_else(|| self.author().name.to_owned())
     }
 
-    async fn get_command_args(self) -> String {
+    async fn get_command_args(&self) -> String {
         match self {
-            poise::Context::Application(ctx) => {
-                let args = ctx
-                    .args
-                    .iter()
-                    .flat_map(|v| format!("{v}").trim_matches('"').to_owned());
-
-                args.join(" ")
-            }
             poise::Context::Prefix(ctx) => ctx.args.to_owned(),
+            poise::Context::Application(ctx) => ctx
+                .args
+                .iter()
+                .map(|v| format!("{:?}", v).trim_matches('"').to_owned())
+                .join(" "),
         }
     }
 
-    async fn get_player(self) -> Result<AudioPlayer, Error> {
+    async fn get_player(&self) -> Result<AudioPlayer> {
         let guild_id = self.assure_guild_context()?;
 
         let jukebox_repository = self.data().repositories.jukebox.to_owned();
 
         let user_id = self.author().id;
 
-        let songbird = get_songbird_client(self).await?;
+        let songbird = get_songbird_client(self.serenity_context()).await?;
 
-        Ok(AudioPlayer::new(
-            guild_id,
-            user_id,
-            songbird,
-            jukebox_repository,
-        ))
+        let manager = AudioManager::new();
+
+        let jukebox = self.data().repositories.jukebox;
+
+        Ok(AudioPlayer::new(guild_id, user_id, manager, jukebox))
     }
 
-    async fn assure_connected(self) -> Result<Option<ChannelId>, Error> {
+    async fn assure_connected(&self) -> Result<Option<ChannelId>> {
         let guild = self.assure_cached_guild()?;
 
         let channel = guild
@@ -73,29 +70,30 @@ impl ContextExt for Context<'_> {
         Ok(channel)
     }
 
-    async fn get_user(self, user_id: u64) -> Result<User, Error> {
+    async fn get_user(&self, user_id: UserId) -> Result<User> {
         let cached_user = self.serenity_context().cache.user(user_id);
 
         let user = match cached_user {
-            Some(u) => Ok(u),
-            None => self.http().get_user(user_id).await,
+            Some(u) => u.to_owned(),
+            None => self.http().get_user(user_id).await?,
         };
 
-        Ok(user?)
+        Ok(user)
     }
 
-    fn assure_cached_guild(self) -> Result<Guild, Error> {
+    fn assure_cached_guild(&self) -> Result<GuildRef<'_>> {
         let guild_id = self.assure_guild_context()?;
+
         self.guild()
             .ok_or_else(|| anyhow!("Couldn't get Guild {} from cache", guild_id))
     }
 
-    fn assure_guild_context(self) -> Result<GuildId, Error> {
+    fn assure_guild_context(&self) -> Result<GuildId> {
         self.guild_id()
             .ok_or_else(|| anyhow!("Context doesn't include an Guild"))
     }
 
-    fn get_guild_info(self) -> Option<GuildInfo> {
+    fn get_guild_info(&self) -> Option<GuildInfo> {
         let guild_id = self.guild_id().map(|g| g.get());
         let guild_name = self.guild_id().and_then(|g| g.name(self));
 
@@ -108,9 +106,7 @@ impl ContextExt for Context<'_> {
     }
 }
 
-pub async fn get_songbird_client(
-    ctx: &poise::serenity_prelude::Context,
-) -> Result<Arc<Songbird>, Error> {
+pub async fn get_songbird_client(ctx: &poise::serenity_prelude::Context) -> Result<Arc<Songbird>> {
     songbird::get(ctx)
         .await
         .ok_or_else(|| anyhow!("Couldn't get songbird voice client"))
--- src/extensions/serenity/guild_ext.rs
+++ src/extensions/serenity/guild_ext.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashMap, sync::Arc};
 
-use anyhow::{anyhow, Error};
+use anyhow::{anyhow, Result};
 use poise::{
     async_trait,
     serenity_prelude::{Cache, GuildId, Http, UserId, VoiceState},
@@ -10,30 +10,30 @@ use crate::extensions::std_ext::collapse_errors;
 
 #[async_trait]
 pub trait GuildExt {
-    async fn say_on_main_text_channel(self, http: &Http, msg: &str) -> Result<(), Error>;
-    fn get_voice_states(self, cache: Arc<Cache>) -> Result<HashMap<UserId, VoiceState>, Error>;
+    async fn say_on_main_text_channel(self, http: &Http, msg: &str) -> Result<()>;
+    fn get_voice_states(self, cache: Arc<Cache>) -> Result<HashMap<UserId, VoiceState>>;
 }
 
 #[async_trait]
 impl GuildExt for GuildId {
-    async fn say_on_main_text_channel(self, http: &Http, msg: &str) -> Result<(), Error> {
+    async fn say_on_main_text_channel(self, http: &Http, msg: &str) -> Result<()> {
         let channels = self.channels(&http).await?;
 
         let channel = channels
             .values()
             .filter(|c| c.is_text_based())
             .min_by(|a, b| a.position.cmp(&b.position))
-            .ok_or_else(|| anyhow!("Guild {} doesn't contain a text channel", self.0))?;
+            .ok_or_else(|| anyhow!("Guild {} doesn't contain a text channel", self.get()))?;
 
         channel.say(http, msg).await?;
 
         Ok(())
     }
 
-    fn get_voice_states(self, cache: Arc<Cache>) -> Result<HashMap<UserId, VoiceState>, Error> {
+    fn get_voice_states(self, cache: Arc<Cache>) -> Result<HashMap<UserId, VoiceState>> {
         let guild = cache
             .guild(self)
-            .ok_or_else(|| anyhow!("Couldn't get guild {} from cache", self.0))?;
+            .ok_or_else(|| anyhow!("Couldn't get guild {} from cache", self.get()))?;
 
         Ok(guild.voice_states)
     }
@@ -42,7 +42,7 @@ impl GuildExt for GuildId {
 pub async fn get_all_online_users(
     http: Arc<Http>,
     cache: Arc<Cache>,
-) -> Result<impl Iterator<Item = StatusInfo>, Error> {
+) -> Result<impl Iterator<Item = StatusInfo>> {
     let guilds_info = http.get_guilds(None, None).await?;
 
     let get_online_users_results = guilds_info.into_iter().map(move |g| {
@@ -51,7 +51,7 @@ pub async fn get_all_online_users(
         let online_users = voice_states
             .into_values()
             .filter(|v| v.channel_id.is_some())
-            .map(move |c| StatusInfo::new(c.user_id.0, g.id.0));
+            .map(move |c| StatusInfo::new(c.user_id.get(), g.id.get()));
 
         Ok(online_users)
     });
