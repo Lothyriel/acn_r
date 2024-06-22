77081df282e8c251c5b4e7f955acf7b1911cc902
João Xavier <fastjonh@gmail.com>
2024-06-22 18:23:25 -0300

    removing unused methods

--- src/extensions/serenity/guild_ext.rs
+++ src/extensions/serenity/guild_ext.rs
@@ -1,22 +1,17 @@
-use std::{collections::HashMap, sync::Arc};
-
 use anyhow::{anyhow, Result};
 use poise::{
     async_trait,
-    serenity_prelude::{Cache, GuildId, Http, UserId, VoiceState},
+    serenity_prelude::{GuildId, Http},
 };
-
-use crate::extensions::std_ext::collapse_errors;
 
 #[async_trait]
 pub trait GuildExt {
-    async fn say_on_main_text_channel(self, http: &Http, msg: &str) -> Result<()>;
-    fn get_voice_states(self, cache: Arc<Cache>) -> Result<HashMap<UserId, VoiceState>>;
+    async fn say_on_main_text_channel(&self, http: &Http, msg: &str) -> Result<()>;
 }
 
 #[async_trait]
 impl GuildExt for GuildId {
-    async fn say_on_main_text_channel(self, http: &Http, msg: &str) -> Result<()> {
+    async fn say_on_main_text_channel(&self, http: &Http, msg: &str) -> Result<()> {
         let channels = self.channels(&http).await?;
 
         let channel = channels
@@ -29,36 +24,6 @@ impl GuildExt for GuildId {
 
         Ok(())
     }
-
-    fn get_voice_states(self, cache: Arc<Cache>) -> Result<HashMap<UserId, VoiceState>> {
-        let guild = cache
-            .guild(self)
-            .ok_or_else(|| anyhow!("Couldn't get guild {} from cache", self.get()))?;
-
-        Ok(guild.voice_states)
-    }
-}
-
-pub async fn get_all_online_users(
-    http: Arc<Http>,
-    cache: Arc<Cache>,
-) -> Result<impl Iterator<Item = StatusInfo>> {
-    let guilds_info = http.get_guilds(None, None).await?;
-
-    let get_online_users_results = guilds_info.into_iter().map(move |g| {
-        let voice_states = g.id.get_voice_states(cache.to_owned())?;
-
-        let online_users = voice_states
-            .into_values()
-            .filter(|v| v.channel_id.is_some())
-            .map(move |c| StatusInfo::new(c.user_id.get(), g.id.get()));
-
-        Ok(online_users)
-    });
-
-    let online_users_results = collapse_errors(get_online_users_results)?;
-
-    Ok(online_users_results.into_iter().flatten())
 }
 
 #[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
