a3b0a5f1129855329abd303db812f1add3c3f01f
João Xavier <fastjonh@gmail.com>
2024-06-22 18:21:31 -0300

    configuring lavalink client in the poise framework building process

--- src/features/mod.rs
+++ src/features/mod.rs
@@ -1,5 +1,9 @@
 use anyhow::{Error, Result};
-use poise::serenity_prelude::{ClientBuilder, GatewayIntents};
+use lavalink_rs::{
+    client::LavalinkClient, model::events::Events, node::NodeBuilder,
+    prelude::NodeDistributionStrategy,
+};
+use poise::serenity_prelude::{ClientBuilder, Context, GatewayIntents};
 use songbird::SerenityInit;
 
 use crate::{
@@ -16,8 +20,10 @@ use self::events::*;
 mod commands;
 mod events;
 
-fn register_commands(groups: Vec<Vec<Command>>) -> Vec<Command> {
+fn register_commands() -> Vec<Command> {
     let mut commands = vec![help::help()];
+
+    let groups = vec![r34::group(), misc::group(), jukebox::group()];
 
     for mut command in groups {
         commands.append(command.as_mut());
@@ -26,17 +32,13 @@ fn register_commands(groups: Vec<Vec<Command>>) -> Vec<Command> {
     commands
 }
 
-fn register_groups() -> Vec<Vec<Command>> {
-    vec![r34::group(), misc::group(), jukebox::group()]
-}
-
 pub async fn start() -> Result<()> {
     let settings = AppSettings::load()?;
     let token = env::get("TOKEN_BOT")?;
 
     ClientBuilder::new(token, GatewayIntents::all())
         .register_songbird()
-        .framework(get_framework(&settings))
+        .framework(get_framework(settings))
         .await?
         .start()
         .await?;
@@ -44,16 +46,44 @@ pub async fn start() -> Result<()> {
     Ok(())
 }
 
-fn get_framework(settings: &AppSettings) -> poise::FrameworkBuilder<DependencyContainer, Error> {
+fn get_framework(settings: AppSettings) -> poise::Framework<DependencyContainer, Error> {
     poise::Framework::builder()
-        .options(get_options(settings))
+        .options(get_options(&settings))
         .setup(|ctx, ready, framework| {
-            Box::pin(async {
+            Box::pin(async move {
                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
 
-                DependencyContainer::build(settings, ready.user.id).await
+                let client = get_lavalink_client(ctx).await?;
+
+                DependencyContainer::build(settings, client, ready.user.id).await
             })
         })
+        .build()
+}
+
+async fn get_lavalink_client(ctx: &Context) -> Result<LavalinkClient> {
+    let events = Events {
+        track_start: None,
+        ..Default::default()
+    };
+
+    let node_local = NodeBuilder {
+        hostname: "192.168.3.10:2333".to_string(),
+        is_ssl: false,
+        events: Events::default(),
+        password: env::get("LAVALINK_PASSWORD")?,
+        user_id: ctx.cache.current_user().id.into(),
+        session_id: None,
+    };
+
+    let client = LavalinkClient::new(
+        events,
+        vec![node_local],
+        NodeDistributionStrategy::round_robin(),
+    )
+    .await;
+
+    Ok(client)
 }
 
 fn get_options(settings: &AppSettings) -> poise::FrameworkOptions<DependencyContainer, Error> {
@@ -61,7 +91,7 @@ fn get_options(settings: &AppSettings) -> poise::FrameworkOptions<DependencyCont
         event_handler: |ctx, event, _, user_data| {
             Box::pin(handlers::invoker::songbird_handler(ctx, event, user_data))
         },
-        commands: register_commands(register_groups()),
+        commands: register_commands(),
         on_error: |error| Box::pin(error::handler(error)),
         post_command: |ctx| Box::pin(after::handler(ctx)),
         prefix_options: poise::PrefixFrameworkOptions {
