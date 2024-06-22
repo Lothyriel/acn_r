927bbb12677738b51adec7d363581a9dd97057f3
João Xavier <fastjonh@gmail.com>
2024-06-22 18:21:56 -0300

    flatting props on the data object

--- src/features/events/check.rs
+++ src/features/events/check.rs
@@ -3,7 +3,7 @@ use anyhow::Result;
 use crate::extensions::serenity::Context;
 
 pub async fn handler(ctx: Context<'_>) -> Result<bool> {
-    let owners = &ctx.data().services.allowed_ids;
+    let owners = &ctx.data().allowed_ids;
     let owners_only = ctx.command().custom_data.downcast_ref::<bool>();
 
     let has_permission = match owners_only {
